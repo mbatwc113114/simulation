ab92fc9f5fd03b9d5ace3d34c851e4ab2efd5f6d
mbatwc113114 <mbatwc@gmail.com>
2025-03-16 00:57:04 +0530

    updated landing page of website

---
+++ Simulation_web/public/js/script.js
@@ -0,0 +1,6 @@
+const themeToggle = document.getElementById('themeToggle');
+        themeToggle.addEventListener('click', () => {
+            const currentTheme = document.documentElement.getAttribute('data-bs-theme');
+            const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+            document.documentElement.setAttribute('data-bs-theme', newTheme);
+        });
